752ec007da163e228247c177a83d9c880ae0d315
atharvsuranje <atharvsuranje2604@gmail.com>
2025-07-17 20:50:17 +0530

    Updated Q6

--- C_module/Assignment 8/Q6.c
+++ C_module/Assignment 8/Q6.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 
 void main() {
-    int n,flag=0;
+    int n;
     printf("Size of array :: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
+
     printf("Enter array Elements :: ");
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
 
     printf("Prime Elements are :: ");
-    for(int i=1;i<n;i++){
-        for(int j=2;j*j<arr[i];j++){
-            if(arr[i]%j == 0){
-                flag=1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < 2) continue;
+        int flag = 0;
+
+        for (int j = 2; j * j <= arr[i]; j++) {
+            if (arr[i] % j == 0) {
+                flag = 1;
                 break;
             }
         }
-        if(flag == 0) printf("%d ",arr[i]);
+
+        if (flag == 0) {
+            printf("%d ", arr[i]);
+        }
     }
 }
