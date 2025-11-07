80d24f7c6c3aaee209bee5449b0d38f22206762f
atharvsuranje <atharvsuranje2604@gmail.com>
2025-11-07 21:15:18 +0530

    Deleted duplicate folder

--- Advanced_Java_module/My_2025_10_31 (Basic Interface)/src/p1/payFees.java
+++
@@ -1,8 +0,0 @@
-package p1;
-public interface payFees {
-	void payOnline();
-	default void payOffline() {
-		System.out.println("Can't do offline payment so doing online payment");
-		payOnline();
-	}
-}
